3528d56df18cc1cc644bfa33ba4b7a72fa9d3d7f
marcocondrache <marcomihaicondrache@gmail.com>
2024-07-08 14:54:42 +0200

    refactor: steps and dialog

--- app/(landing)/experience/page.tsx
+++ app/(landing)/experience/page.tsx
@@ -1,36 +1,13 @@
-import { PropsWithChildren } from "react";
 import { SewingPinFilledIcon } from "@radix-ui/react-icons";
 
 import { Card, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { ExternalLink } from "@/components/external-link";
+import { Step, Steps } from "@/components/steps";
 
 export const metadata = {
   robots: "noindex,nofollow",
 };
-
-type StepProps = PropsWithChildren<{
-  title: string;
-  from: Date;
-  to: Date | "present";
-}>;
-
-function Step({ children, title, from, to }: StepProps) {
-  return (
-    <>
-      <div className="step mt-8 flex flex-row items-center justify-between">
-        <h3 className="scroll-m-20 text-lg font-medium tracking-tight">
-          {title}
-        </h3>
-        <span className="ml-2 text-xs text-stone-400">
-          {from.toLocaleDateString()}&nbsp;-&nbsp;
-          {to === "present" ? "present" : to.toLocaleDateString()}
-        </span>
-      </div>
-      {children}
-    </>
-  );
-}
 
 export default function Page() {
   return (
@@ -55,7 +32,9 @@ export default function Page() {
             <div className="flex w-full flex-row items-center justify-between">
               <div className="flex flex-row flex-wrap gap-3">
                 <ExternalLink href="https://github.com">github</ExternalLink>
-                <ExternalLink href="https://github.com">linkedin</ExternalLink>
+                <ExternalLink href="https://www.linkedin.com/in/marco-mihai-condrache/">
+                  linkedin
+                </ExternalLink>
                 <ExternalLink href="https://github.com/mossida">
                   mossida
                 </ExternalLink>
@@ -68,10 +47,10 @@ export default function Page() {
           </CardContent>
         </Card>
         <h2 className="text-xl">Work experience</h2>
-        <div className="[&>div.step]:step mb-12 ml-4 border-l pl-8">
+        <Steps>
           <Step
             title="Junior Software Developer"
-            from={new Date("2023-09-18")}
+            from={"2023-09-18"}
             to="present"
           >
             <h4 className="text-sm text-stone-500">
@@ -92,8 +71,8 @@ export default function Page() {
 
           <Step
             title="Embedded Software Developer"
-            from={new Date("2021-09-01")}
-            to={new Date("2022-12-01")}
+            from={"2021-09-01"}
+            to={"2022-12-01"}
           >
             <h4 className="text-sm text-stone-500">
               at <em>XTEE</em>
@@ -110,63 +89,62 @@ export default function Page() {
               </p>
             </div>
           </Step>
-        </div>
+        </Steps>
         <h2 className="text-xl">Education</h2>
-        <div className="[&>h3]:step mb-12 ml-4 border-l pl-8">
-          <h3 className="mt-8 scroll-m-20 text-lg font-medium tracking-tight">
-            Bachelor&apos;s Degree in Computer Science
-          </h3>
-          <h4 className="text-sm text-stone-500">
-            at <em>Università Ca Foscari</em>
-          </h4>
-          <div className="mt-3 space-y-3 text-sm">
-            <p>
-              I&apos;m completing a curriculum with a strong focus on computer
-              science and mathematics. I&apos;m excelling in higher-level math
-              courses, including calculus and statistics.
-            </p>
-            <p>
-              Participated in specialized computer science classes covering
-              programming fundamentals, data structures, and computer
-              architecture
-            </p>
-          </div>
-          <h3 className="mt-8 scroll-m-20 text-lg font-medium tracking-tight">
-            Erasmus+
-          </h3>
-          <h4 className="text-sm text-stone-500">
-            at <em>SeviManager</em>
-          </h4>
-          <div className="mt-3 space-y-3 text-sm">
-            <p>
-              I&apos;created custom messaging and contact management software,
-              collaborating with owners to design, implement, and integrate the
-              solution.
-            </p>
-            <p>
-              They provided testing, training, support, and documentation
-              throughout the project.
-            </p>
-          </div>
-          <h3 className="mt-8 scroll-m-20 text-lg font-medium tracking-tight">
-            Diploma in Computer Science
-          </h3>
-          <h4 className="text-sm text-stone-500">
-            at <em>ITTS Vito Volterra (High School)</em>
-          </h4>
-          <div className="mt-3 space-y-3 text-sm">
-            <p>Graduated with honors.</p>
-            <p>
-              Advanced curriculum emphasizing computer science and mathematics.
-              Excelled in high-level math courses. Specialized classes in
-              programming and computer architecture.
-            </p>
-            <p>
-              Demonstrated strong problem-solving skills and mathematical
-              aptitude.
-            </p>
-          </div>
-        </div>
+        <Steps>
+          <Step title="Bachelor's Degree in Computer Science">
+            <h4 className="text-sm text-stone-500">
+              at <em>Università Ca Foscari</em>
+            </h4>
+            <div className="mt-3 space-y-3 text-sm">
+              <p>
+                I&apos;m completing a curriculum with a strong focus on computer
+                science and mathematics. I&apos;m excelling in higher-level math
+                courses, including calculus and statistics.
+              </p>
+              <p>
+                Participated in specialized computer science classes covering
+                programming fundamentals, data structures, and computer
+                architecture
+              </p>
+            </div>
+          </Step>
+
+          <Step title="Erasmus+">
+            <h4 className="text-sm text-stone-500">
+              at <em>SeviManager</em>
+            </h4>
+            <div className="mt-3 space-y-3 text-sm">
+              <p>
+                I&apos;created custom messaging and contact management software,
+                collaborating with owners to design, implement, and integrate
+                the solution.
+              </p>
+              <p>
+                They provided testing, training, support, and documentation
+                throughout the project.
+              </p>
+            </div>
+          </Step>
+
+          <Step title="Diploma in Computer Science">
+            <h4 className="text-sm text-stone-500">
+              at <em>ITTS Vito Volterra (High School)</em>
+            </h4>
+            <div className="mt-3 space-y-3 text-sm">
+              <p>Graduated with honors.</p>
+              <p>
+                Advanced curriculum emphasizing computer science and
+                mathematics. Excelled in high-level math courses. Specialized
+                classes in programming and computer architecture.
+              </p>
+              <p>
+                Demonstrated strong problem-solving skills and mathematical
+                aptitude.
+              </p>
+            </div>
+          </Step>
+        </Steps>
       </section>
     </>
   );
