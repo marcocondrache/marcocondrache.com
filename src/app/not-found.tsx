134e87256e7515d928256dfb9151d8f26bd44626
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-03-31 15:23:11 +0200

    refactor: initialize astro

--- src/app/not-found.tsx
+++
@@ -1,3 +0,0 @@
-export default function NotFound() {
-  return <h1 className="mb-8 text-2xl tracking-tighter">are you lost?</h1>;
-}
--- src/lib/links.ts
+++
@@ -1,7 +0,0 @@
-export const links = {
-  baseURL: new URL(process.env.NEXT_PUBLIC_URL ?? "http://localhost:3000"),
-  x: "https://x.com/marcocondrache",
-  linkedin: "https://www.linkedin.com/in/marcocondrache/",
-  commit: `https://github.com/marcocondrache/marcocondrache.com/commit/${process.env.NEXT_PUBLIC_GIT_HASH}`,
-  github: "https://github.com/marcocondrache/marcocondrache.com",
-};
