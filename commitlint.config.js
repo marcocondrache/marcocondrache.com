42c665a2e6cc12ae4f24ca4a0956e4937df59820
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-04-02 08:44:26 +0200

    ci: use correct commit message

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,9 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 500],
   },
+  ignores: [
+    (commit) => commit.includes('[skip ci]')
+  ]
 }; 
