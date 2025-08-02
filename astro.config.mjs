311d300e069fb71675a180c0790ebbaff2792612
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-08-02 20:49:04 +0200

    feat: serve only static files
    Signed-off-by: Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,26 +5,13 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   site: 'https://marcocondrache.com',
   integrations: [react(), sitemap()],
 
   vite: {
     // @ts-expect-error
     plugins: [tailwindcss()],
-    // https://github.com/withastro/astro/issues/12824
-    resolve: {
-      alias: import.meta.env.PROD
-        ? {
-            'react-dom/server': 'react-dom/server.edge',
-          }
-        : undefined,
-    },
   },
-
-  adapter: cloudflare(),
 });
